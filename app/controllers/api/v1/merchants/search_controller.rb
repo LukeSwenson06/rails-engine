d79c934d072df01662bd465903d0282373d3217b
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-14 14:26:43 -0600

    added functionalilty to index action

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def index
-
+    search = Merchant.search_merchant(params[:name])
+    render json: MerchantSerializer.new(search)
   end
 end
