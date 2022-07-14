ff38080be302d185dd63bf63cc2520892e9776af
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-14 13:13:52 -0600

    created merchant search model test

--- spec/models/merchants_spec.rb
+++ spec/models/merchants_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+    it "can use cap insenstive searchs for 1 merchant and returns the first one in alphabetical order" do
+      merchant1 = create(:merchant, name: "Alpinaud Leveilleur")
+      merchant2 = create(:merchant, name: "Almeric de Borel")
+      merchant3 = create(:merchant, name: "Thancred Waters")
+      merchant4 = create(:merchant, name: "Y'shtola Rhul")
+
+      expect(Merchant.search_merchant("aLpI")).to eq([merchant1])
+      expect(Merchant.search_merchant("al")).to eq([merchant2])
+
+  end
 end
