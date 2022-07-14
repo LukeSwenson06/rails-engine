9ca78e9e3f2437e5d3cbee625389c8119aa0a188
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-14 13:21:29 -0600

    added search one merchant model method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.search_merchant(search_params)
+     where("name ILIKE ?", "%#{search_params}%")
+     .order(name: :asc)
+     .limit(1)
+  end
 end
