b5d81fc4f062268b354aa0c7beb35d86511d425e
Luke Swenson <LukeSwenson95@outlook.com>
2022-07-14 19:59:04 -0600

    Created search tests for items

--- spec/requests/api/v1/items/search_spec.rb
+++ spec/requests/api/v1/items/search_spec.rb
@@ -5,15 +5,20 @@ RSpec.describe 'Item Search' do
     merchant = create(:merchant)
     item1 = create(:item, name: "The Shadows Ring", merchant: merchant)
     item2 = create(:item, name: "ring a ding", merchant: merchant)
-    item2 = create(:item, name: "Bracers", merchant: merchant)
-    item2 = create(:item, name: "Anklet", merchant: merchant)
+    item3 = create(:item, name: "Bracers", merchant: merchant)
+    item4 = create(:item, name: "Anklet", merchant: merchant)
 
     get '/api/v1/items/find_all?name=rInG'
 
     response_body = JSON.parse(response.body, symbolize_names: true)
-
     expect(response).to be_successful
 
+    items = response_body[:data].map do |item|
+      item[:attributes][:name]
+    end
+
+    expect(items).to eq([item1.name, item2.name])
+    expect(items).to_not eq([item3.name, item4.name])
 
   end
 end
